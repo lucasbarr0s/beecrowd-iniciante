4bdc2d9b9c1e964a65dcc1dae24cbfcc7a049587
Lucas <148596941+lucasbarr0s@users.noreply.github.com>
2023-12-26 10:56:39 -0300

    Create 1051_impostoderenda.c

---
+++ 1051_impostoderenda.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    double salario;
+    scanf("%lf", &salario);
+
+    double imposto = 0.0;
+
+    if (salario <= 2000.00) {
+        printf("Isento\n");
+    } else {
+        if (salario <= 3000.00) {
+            imposto = (salario - 2000.00) * 0.08;
+        } else if (salario <= 4500.00) {
+            imposto = 1000.00 * 0.08 + (salario - 3000.00) * 0.18;
+        } else {
+            imposto = 1000.00 * 0.08 + 1500.00 * 0.18 + (salario - 4500.00) * 0.28;
+        }
+
+        printf("R$ %.2lf\n", imposto);
+    }
+
+    return 0;
+}
