2b8a39780fe7056e88a72a27d8b277887adb32c8
Lucas <148596941+lucasbarr0s@users.noreply.github.com>
2023-12-26 10:11:47 -0300

    Create 1043_triangulo.c

---
+++ 1043_triangulo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float A, B, C;
+
+    scanf("%f %f %f", &A, &B, &C);
+
+    if (A + B > C && A + C > B && B + C > A) {
+        float perimetro = A + B + C;
+        printf("Perimetro = %.1f\n", perimetro);
+    } else {
+        float area_trapezio = ((A + B) * C) / 2;
+        printf("Area = %.1f\n", area_trapezio);
+    }
+}
